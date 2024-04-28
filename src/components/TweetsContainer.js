611630987b336e6faffcee477891462b2b34bd50
Makena Sandra <47531164+makenasandra@users.noreply.github.com>
2024-04-28 19:19:00 +0000

    Click 'View Tweets' on the user card to view the user's tweets

--- src/components/TweetsContainer.js
+++ src/components/TweetsContainer.js
@@ -5,6 +5,12 @@ import { users as userData } from "../data/data";
 
 function TweetsContainer() {
   const [users, setUsers] = useState(userData);
+  const [currentUser, setCurrentUser] = useState(users[0]);
+
+  function handleViewTweets(user){
+    console.log("u clicked the user", user.handle);
+    setCurrentUser(user);
+  }
 
   console.log("In TweetsContainer, state is", users);
   return (
@@ -12,11 +18,11 @@ function TweetsContainer() {
       <div className="ui grid">
         <div className="six wide column">
           <h2 className="ui header">Users</h2>
-          <UserList users={users} />
+          <UserList users={users} handleViewTweets={handleViewTweets}/>
         </div>
         <div className="ten wide column">
           <h2 className="ui header">Tweets</h2>
-          <TweetList user={users[0]} />
+          <TweetList user={currentUser} />
         </div>
       </div>
     </div>
